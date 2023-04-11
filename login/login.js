c44cfe7b465a2d0cd839c6d35cf10bd704f9cbac
BTG321 <130459911+BTG321@users.noreply.github.com>
2023-04-11 08:54:31 -0300

    Add files via upload

---
+++ login/login.js
@@ -0,0 +1,11 @@
+function validarFormulario() {
+    var campo1 = document.getElementById("campo1").value;
+    var campo2 = document.getElementById("campo2").value;
+    if (campo1 == "" || campo2 == "") {
+        console.log("Funfo")
+        alert("Por favor, preencha todos os campos.");
+    } else {
+        
+    }
+}
+
