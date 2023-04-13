ab2632bce290111d22964a8fe2ad1d6582aac8db
BTG321 <130459911+BTG321@users.noreply.github.com>
2023-04-13 15:02:44 -0300

    Add files via upload

---
+++ security/security.js
@@ -0,0 +1,29 @@
+var header = document.getElementById('header');
+var navigationHeader = document.getElementById('navigation_header');
+var showSidebar = false;
+
+
+function toggleSidebar() {
+    showSidebar = !showSidebar;
+    if(showSidebar) {
+        navigation_header.style.marginLeft = '-10vw';
+        navigation_header.style.animationName = 'showSidebar'
+        content.style.filter = blur(2);
+    } else {
+        navigation_header.style.marginLeft = '-100vw'
+        navigation_header.style.animationName = ''
+        content.style.filter = '';
+    }
+}
+
+function closeSidebar() {
+    if(showSidebar) {
+        toogleSidebar();
+    }
+}
+
+window.addEventListener('resize', function(event) {
+    if(window.innerWidth > 768 && showSidebar){
+        toogleSidebar();
+    }
+});
