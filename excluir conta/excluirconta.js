c6b79267c3dac6df4da84294e8b787a2a08baab8
BTG321 <130459911+BTG321@users.noreply.github.com>
2023-04-14 17:24:16 -0300

    Add files via upload

--- excluir conta/excluirconta.js
+++ excluir conta/excluirconta.js
@@ -27,3 +27,24 @@ window.addEventListener('resize', function(event) {
         toogleSidebar();
     }
 });
+
+function clicar(){
+    let botao = document.getElementById('btn1')
+    let container = document.querySelector('.aviso')
+
+    if(container.style.display == 'block'){
+        container.style.display = 'none'
+    } else {
+        container.style.display = 'block'
+    }
+}
+
+function redirecionar(){
+    window.location.href = "../settings/settings.html"
+}
+
+function cancelar(){
+    let container = document.querySelector('.aviso')
+
+    container.style.display = 'none'
+}
