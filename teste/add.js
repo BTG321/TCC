92bc8ee695ac2a58c5979fc800d2f5a3818668b4
BTG321 <130459911+BTG321@users.noreply.github.com>
2023-04-14 07:39:52 -0300

    Add files via upload

--- teste/add.js
+++ teste/add.js
@@ -27,3 +27,19 @@ window.addEventListener('resize', function(event) {
         toogleSidebar();
     }
 });
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+    nextImage();
+}, 2000)
+
+function nextImage(){
+    count++;
+    if(count>4){
+        count = 1;
+    }
+
+    document.getElementById("radio"+count).checked = true;
+}
