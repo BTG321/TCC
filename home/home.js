0437325bed1d7d5c36d05494d4bccfb92fd21632
BTG321 <130459911+BTG321@users.noreply.github.com>
2023-04-27 08:44:15 -0300

    Add files via upload

--- home/home.js
+++ home/home.js
@@ -39,3 +39,16 @@ radios.forEach((radio) => {
     unselected.style.borderColor = "#ccc";
   });
 });
+
+
+function validarFormulario() {
+    event.preventDefault()
+     var campo1 = document.getElementById("campo1").value;
+      var campo2 = document.getElementById("campo2").value;
+      var campo2 = document.getElementById("campo3").value;
+      if (campo1 == "" || campo2 == "" || campo3 == "") {
+        console.log("Funfo")
+           alert("Por favor, preencha todos os campos.");
+     } else
+     window.location.href="../home/home-add.html";
+   }
