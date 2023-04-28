6825384acc92d8008c26f67563e5bf5998dedf98
BTG321 <130459911+BTG321@users.noreply.github.com>
2023-04-28 11:33:37 -0300

    Add files via upload

--- esqueci/novasenha.js
+++ esqueci/novasenha.js
@@ -1,11 +1,12 @@
 function aviso() {
+    event.preventDefault()
     let campo1 = document.getElementById('code')
     let campo2 = document.getElementById('senha') 
 
     if (campo1.value == 0 || campo2.value == 0) {
         window.alert('Preencha todos os campos para prosseguir!')
     } else {
-        window.location.replace("https://btg321.github.io/TCC/home/home-bibliotea.html");
+        window.location.replace("../home/home-bibliotea.html");
 
     }
 }
