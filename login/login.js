c16f22d59761b01d7f2c9b95efe82ca6496aa5f8
BTG321 <130459911+BTG321@users.noreply.github.com>
2023-06-30 10:18:54 -0300

    Add files via upload

--- login/login.js
+++ login/login.js
@@ -4,7 +4,6 @@
   var campo1 = document.getElementById("campo1").value;
    var campo2 = document.getElementById("campo2").value;
    if (campo1 == "" || campo2 == "") {
-     console.log("Funfo")
         alert("Por favor, preencha todos os campos.");
   } else
   window.location.href="../home/home-bibliotea.html";
