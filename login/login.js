07b98f62814f3fbb760e06e932c6f9768e0c06a2
BTG321 <130459911+BTG321@users.noreply.github.com>
2023-08-05 12:28:33 -0300

    Add files via upload

--- login/login.js
+++ login/login.js
@@ -1,13 +1,14 @@
+let entrar = document.getElementById('bnt-submit')
+let campo1 = document.getElementById("campo1")
+let campo2 = document.getElementById("campo2")
 
- function validarFormulario() {
- event.preventDefault()
-  var campo1 = document.getElementById("campo1").value;
-   var campo2 = document.getElementById("campo2").value;
-   if (campo1 == "" || campo2 == "") {
-        alert("Por favor, preencha todos os campos.");
-  } else
-  window.location.href="../home/home-bibliotea.html";
-}
+entrar.addEventListener('click', function(){
+  if (campo1.value == '' || campo2.value == '') {
+    window.alert("Por favor, preencha todos os campos.");
+  } else {
+    window.location.href="../home/home-bibliotea.html";
+  }
+})
 
 
 
