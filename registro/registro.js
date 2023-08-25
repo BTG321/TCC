b18e8cd3ef2da2ca421a41ab2c4770d577bc61ae
BTG321 <130459911+BTG321@users.noreply.github.com>
2023-08-25 19:24:37 -0300

    Add files via upload

--- registro/registro.js
+++ registro/registro.js
@@ -1,8 +1,4 @@
 let submit = document.querySelector('.a')
-let user = document.getElementById('campo1')
-let senha = document.getElementById('campo2')
-let email = document.getElementById('campo3')
-let telefone = document.getElementById('campo4')
 
 submit.addEventListener('click', function(){
     window.location.href="../emailencaminhado/emailencaminhado.html";
