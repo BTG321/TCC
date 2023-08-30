0cd261035c139ba7710b51fe6ec2074d63af672b
BTG321 <130459911+BTG321@users.noreply.github.com>
2023-08-30 12:03:25 -0300

    Add files via upload

--- registro/registro.js
+++ registro/registro.js
@@ -1,8 +1,15 @@
 let submit = document.querySelector('.a')
+let campo1 = document.getElementById("campo1")
+let campo2 = document.getElementById("campo2")
+let campo3 = document.getElementById("campo3")
+let campo4 = document.getElementById("campo4")
 
 submit.addEventListener('click', function(){
-    event.preventDefault()
-    window.location.href="../emailencaminhado/emailencaminhado.html";
+  if (campo1.value == '' || campo2.value == '' || campo3.value == '' || campo4.value == '') {
+    window.alert("Por favor, preencha todos os campos.");
+  } else {
+    window.location.href="../home/home-bibliotea.html";
+  }
 })
    
    
