5ea3593241eca84dc17e77f095fa231b2fb85f6e
BTG321 <130459911+BTG321@users.noreply.github.com>
2023-09-04 12:30:00 -0300

    Update registro.js

--- registro/registro.js
+++ registro/registro.js
@@ -5,6 +5,7 @@ let campo3 = document.getElementById('campo3').value
 let campo4 = document.getElementById('campo4').value
 
 submit.addEventListener('click', function(){
+    event.preventDefault()
     if(campo1 == '' || campo2 == '' || campo3 == '' || campo4 == '') {
         window.alert('Preencha todos os campos!')
     } else {
