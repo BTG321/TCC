49629c7503891b2f52174296cb5261adefc0df8b
BTG321 <130459911+BTG321@users.noreply.github.com>
2023-09-18 09:47:19 -0300

    Add files via upload

--- login/login.js
+++ login/login.js
@@ -3,6 +3,7 @@ let campo1 = document.getElementById("campo1")
 let campo2 = document.getElementById("campo2")
 
 entrar.addEventListener('click', function(){
+  event.preventDefault()
   if (campo1.value == '' || campo2.value == '') {
     window.alert("Por favor, preencha todos os campos.");
   } else {
--- registro/registro.js
+++ registro/registro.js
@@ -1,17 +1,19 @@
-let submit = document.querySelector('.a')
-let campo1 = document.getElementById('campo1').value
-let campo2 = document.getElementById('campo2').value
-let campo3 = document.getElementById('campo3').value
-let campo4 = document.getElementById('campo4').value
+let submit = document.querySelector('.a');
+let campo1 = document.getElementById('campo1');
+let campo2 = document.getElementById('campo2');
+let campo3 = document.getElementById('campo3');
+let campo4 = document.getElementById('campo4');
 
 submit.addEventListener('click', function(){
-    if(campo1 == '' || campo2 == '' || campo3 == '' || campo4 == '') {
-        window.alert('Preencha todos os campos!')
+    event.preventDefault()
+    if(campo1.value == '' || campo2.value == '' || campo3.value == '' || campo4.value == '') {
+        window.alert('Preencha todos os campos!');
     } else {
         window.location.href="../emailencaminhado/emailencaminhado.html";
     }
 })
-   
+
+
    
    
    
